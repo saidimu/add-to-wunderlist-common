ca33705329b319468953366fa6a8108ca8d40cc0
Raymond May Jr <raymondmay@gmail.com>
2013-02-25 12:55:16 +0100

    parse opengraph and twitter card, and prioritize data extraction

--- WL.js
+++ WL.js
@@ -14,25 +14,65 @@
     WL = window.WL;
   }
 
+  function fetchOpenGraph () {
+
+    var data = {};
+
+    data.url = $('meta[name="og:url"]').attr('content');
+    data.title = $('meta[name="og:title"]').attr('content');
+    data.description = $('meta[name="og:description"]').attr('content');
+
+    console.log(data);
+
+    return data;
+  }
+
+  function fetchTwitterCard () {
+
+    var data = {};
+
+    data.url = $('meta[name="twitter:url"]').attr('content');
+    data.title = $('meta[name="twitter:title"]').attr('content');
+    data.description = $('meta[name="twitter:description"]').attr('content');
+
+    console.log(data);
+
+    return data;
+  }
+
   function buildUrl (data) {
 
     // Builds url for passing data to wunderlist.com extension frame.
     // Takes passes in data, or defaults to the tabs title and url or text selection in the frame
 
-    var title = data.title || encodeURIComponent(document.title);
-    var note = data.note || window.location.href;
-    var selection = window.getSelection().toString();
+    // fetch open graph and twitter card meta data
+    var openGraph = fetchOpenGraph();
+    var twitterCard = fetchTwitterCard();
 
+    // build main meta datas - left priority
+    var title = data.title || openGraph.title || twitterCard.title || document.title;
+    var description = openGraph.description || twitterCard.description || $('meta[name="description"]').attr('content');
+    var url = openGraph.url || twitterCard.url || $('link[rel="canonical"]').attr('href') || window.location.href;
+
+    // start building note
+    var note = data.note || url;
+    var selection = window.getSelection().toString();
+    // if not passed in note data use a default constructor
     if (!data.note) {
 
-      note = note + (selection ? " \n\n" + selection : " \n\n" + $('meta[name="description"]').attr('content'));
+      // append desciption after url in note
+      // use selection over description if present
+      note = note + (selection ? " \n\n" + selection : " \n\n" + description);
     }
 
+    // prepare specialList data if present
     if (data.specialList) {
 
       note = 'specialList:' + data.specialList + '\u2603' + note;
     }
 
+    // encode
+    title = encodeURIComponent(title);
     note = encodeURIComponent(note);
 
     return data.config.host + '/#/extension/add/' + title + '/' + note;
